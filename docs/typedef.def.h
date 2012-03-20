1b63b0ec0e0e7447485b7a6a29f4fdf3731deb67
Albert Varaksin <albeva@me.com>
2012-03-20 18:23:19 +0000

    new type definition map in progress

---
+++ docs/typedef.def.h
@@ -0,0 +1,32 @@
+//
+// Info needed:
+// - parsable: include in lexer and parser ad a keyword?
+// - sizes for 32bit build
+// - sizes for 64bit build
+// - kind: FloatingPoint, Integral, Pointer, Function
+// - vararg behaviour
+// - return type
+// - function param type
+#define DEFINE_TYPE(_)                                                        \
+    /* Id       String      32{S, F, V}     64{S, F, V},    Kind
+    -----------------------------------------------------------------------*/ \
+    _( Byte,    "BYTE",     {8,  8,  32},   {8,  8,  32},   Signed          ) \
+    _( UByte,   "UBYTE",    {8,  8,  32},   {8,  8,  32},   Unsigned        ) \
+    _( Short,   "SHORT",    {16, 16, 32},   {16, 16, 32},   Signed          ) \
+    _( UShort,  "USHORT",   {16, 16, 32},   {16, 16, 32},   Unsigned        ) \
+    _( Integer, "INTEGER",  {32, 32, 32},   {32, 32, 32},   Signed          ) \
+    _( Uinteger,"UINTEGER", {32, 32, 32},   {32, 32, 32},   Unsigned        ) \
+    _( Long,    "LONG",     {32, 32, 32},   {64, 64, 64},   Signed          ) \
+    _( ULong,   "ULONG",    {32, 32, 32},   {64, 64, 64},   Unsigned        ) \
+    _( LongInt, "LONGINT",  {64, 64, 64},   {64, 64, 64},   Signed          ) \
+    _( ULongInt,"ULONGINT", {64, 64, 64},   {64, 64, 64},   Unsigned        ) \
+    _( Single,  "SINGLE",   {32, 32, 64},   {32, 32, 64},   FloatingPoint   ) \
+    _( Double,  "DOUBLE",   {64, 64, 32},   {64, 64, 64},   FloatingPoint   ) \
+    _( Decimal, "DECIMAL",  {80, 80, 80},   {80, 80, 80},   FloatingPoint   ) \
+    _( Bool,    "BOOL",     {8,  1,  32},   {8,  1,  32},   Boolean         )
+
+#define CONSTANT_VALUE(_) \
+    _( Null,    "NULL",     AnyPtr,     0) \
+    _( True,    "TRUE",     Bool,       1) \
+    _( False,   "FALSE",    Bool,       0)
+        
